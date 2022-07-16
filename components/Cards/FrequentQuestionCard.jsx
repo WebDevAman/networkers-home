0930e3a7d3b9f5fd8904c056ff72917eaa2b8cb1
Sadath Syed <sadathsadu2002@gmail.com>
2022-07-16 21:38:24 +0530

    first commit

---
+++ components/Cards/FrequentQuestionCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FrequentQuestionCard = () => {
+  return (
+    <div>FrequentQuestionCard</div>
+  )
+}
+
+export default FrequentQuestionCard
---
+++ components/Testimonials/FrequentQuestions/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FrequentQuestions = () => {
+  return (
+    <div>FrequentQuestions</div>
+  )
+}
+
+export default FrequentQuestions
